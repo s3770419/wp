4e4b8d5087f02de1d4756743026b4520cb574652
s3770419 <s3770419@student.rmit.edu.au>
2019-01-23 15:25:58 +1100

    Commit requested by Trevor

--- a2/index.php
+++ a2/index.php
@@ -3,23 +3,316 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Assignment 2</title>
-    
-    <!-- Keep wireframe.css for debugging, add your css to style.css -->
+
+    <title>Lunardo Cinema</title>
     <link id='wireframecss' type="text/css" rel="stylesheet" href="../wireframe.css" disabled>
-    <link id='stylecss' type="text/css" rel="stylesheet" href="css/style.css">
-    <script src='../wireframe.js'></script>
-  </head>
+    <link rel="stylesheet" type="text/css" href="style.css">
+ 
+</head>
 
   <body>
 
     <header>
-      <div>Put company logo and name here</div>
-    </header>
+      <div><img src='../../media/logo.jpg'/> </div>
+    </header></p>
 
-    <nav>
-      <div>Put website navigation links here</div>
+
+    <nav class="navbar" id="navbar">
+    <ul>
+    <li><a class="active" href="#aboutus">About Us</a></li>
+    <li><a href="#pricelist">Price list</a></li>
+    <li><a href="#nowshowing">Now Showing</a></li>
+    <li><a href="#booking">Bookings</a></li>
+    </ul>
     </nav>
+
+<div class="aboutus" id="aboutus">
+    <section>
+    <a name="aboutus"></a>
+    <br>
+    <h1>About Us</h1>
+    Lunardo is a locally owned cinema playing new release movies. After a recent redevelopment the cinema has now
+    upgraded its' facilities to give customers better viewing options and an improved visual and audio experience.
+<p>
+    <strong>Seating</strong>
+    The cinema has received extensive renovations to its' interior, and now offers improved seating options. These include
+    standard seats and reclinable first class seats.
+<p>
+    <img src='../../media/projectimage1.jpg' height="120" width="120"/>
+	<br>
+        	<strong>standard seats</strong>
+<p>
+    <img src='../../media/projectimage2.jpg'height="120" width="120"/>
+        <br>
+       		 <strong>Reclinable first class seats</strong>
+<p>
+	<strong>3D Dolby Vision projection and Dolby Atmos sound</strong>
+<br>
+The cinema has upgraded its project and audio facilities to provide state of the art visual projection from the Dolby 3d Vision Projection
+hardware, giving the cinema the capability of delivering 3d movies, as well sound from the Dolby Atmos hardware that delivers amazing sorround sound.
+
+    </section>
+</div>
+
+
+<div class="pricelist" id="pricelist">
+    <section>
+        <a name="pricelist"></a>
+        <br>
+        <h1>Price list</h1>
+<p>
+<strong>Prices</strong>
+<p>
+The table below shows the prices
+    <br>
+    <table>
+        <caption> <strong> Ticket classes and prices</strong> </caption>
+    <tbody>
+        <tr>
+            <td><strong>Seat Type<strong></td>
+            <td><strong>Seat Code<strong></td>
+            <td><strong>Discount class #1</strong></td>
+            <td><strong>Discount class #2</strong></td>
+        </tr>
+        <tr>
+            <td><strong>Standard Adult</strong></td>
+            <td>&nbsp;STA</td>
+            <td>14.00</td>
+            <td>19.80</td>
+        </tr>
+        <tr>
+            <td><strong>Standard Concession</strong></td>
+            <td>&nbsp;STP</td>
+            <td>12.50</td>
+            <td>&nbsp;&nbsp;7.50</td>
+        </tr>
+        <tr>
+            <td><strong>Standard Child</strong></td>
+            <td>&nbsp;STC</td>
+            <td>11.00</td>
+            <td>15.30</td>
+        </tr>
+        <tr>
+            <td><strong>First Class Adult</strong></td>
+            <td>&nbsp;FCA</td>
+            <td>24.00</td>
+            <td>30.00</td>
+        </tr>
+        <tr>
+            <td><strong>First Class Concession</strong></td>
+            <td>&nbsp;FCP</td>
+            <td>22.50</td>
+            <td>27.00</td>
+        </tr>
+        <tr>
+            <td><strong>First Class Child</strong></td>
+            <td>FCC&nbsp;&nbsp;</td>
+            <td>21.00</td>
+            <td>24.00</td>
+            </tr>
+    </tbody>
+</table>
+<br>
+    <strong> * Discount class #1 refers to all day Monday, Wednesday, and 12pm on weekends
+<br>
+** Discount class #2 refers to all other times </strong>
+    </section>
+</div>
+
+
+<div class="nowshowing" id="nowshowwing">
+    <section>
+        <a name="nowshowing"></a>
+<br>
+    <h1>Now Showing</h1>
+<p>
+Click a movie title for detailed synopsis.
+<p>
+To make a reservation use the bookings section
+<p>
+
+    <strong>The Girl in the Spider's Web</strong>
+<br>
+        <img src='../../media/tgsw.jpg' height="320" width="200"/>
+<br>
+<br>
+Claire Foy, Beau Gadsdon, Sverrir Gudnason
+<p>
+    Wednesday - 9pm (21:00)
+    Thursday - 9pm (21:00)
+    Friday - 9pm (21:00)
+    Saturday - 6pm (18:00)
+    Sunday - 6pm (18:00)
+
+<details>
+<summary><strong>More details, show times and bookings</strong></summary>
+
+<p>
+<h1>The Girl in the Spider's Web (2018)</h1>
+    Claire Foy, Beau Gadsdon, Sverrir Gudnason
+<br>
+    Young computer hacker Lisbeth Salander and journalist Mikael Blomkvist find themselves caught in a web of spies, cybercriminals and corrupt government officials.
+<br>
+<br>
+<iframe width="560" height="315"
+    src="https://www.youtube.com/embed/XKMSP9OKspQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></p>
+</p>
+<strong>Book now:</strong>
+ <input id="mbutton" type="button" value="Wednesday - 9pm (21:00)" />
+ <input id="mbutton" type="button" value="Thursday - 9pm (21:00)" />
+ <input id="mbutton" type="button" value="Friday - 9pm (21:00)" />
+ <input id="mbutton" type="button" value="Saturday - 6pm (18:00)" />
+ <input id="mbutton" type="button" value="Sunday - 6pm (18:00)" />
+ </details>
+<p>
+
+<strong>A Star is Born</strong>
+<br>
+    <img src='../../media/asib.png' height="320" width="200"/>
+<br>
+<br>
+    Lady Gaga, Bradley Cooper, Sam Elliott
+</p>
+   Monday - 6pm (18:00)
+   Tuesday - 6pm (18:00)
+   Saturday - 3pm (15:00)
+   Sunday - 3pm (15:00)
+
+<details>
+    <summary><strong>More details, show times and bookings</strong></summary>
+
+<br>
+    Lady Gaga, Bradley Cooper, Sam Elliott
+<br>
+    A musician helps a young singer find fame, even as age and alcoholism send his own career into a downward spiral.
+<br>
+<br>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/nSbzyEJ8X9E" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <strong>Book now:</strong>
+<br>
+ <input id="mbutton" type="button" value="Monday - 6pm (18:00)" />
+ <input id="mbutton" type="button" value="Tuesday - 6pm (18:00)" />
+ <input id="mbutton" type="button" value="Saturday - 3pm (15:00)" />
+ <input id="mbutton" type="button" value="Sunday - 3pm (15:00)" />
+</details>
+</div>
+<p>
+<div class="w3-panel w3-blue w3-card-4">
+<strong>Ralph Breaks the Internet</strong>
+<br>
+    <img src='../../media/rbti.png' height="320" width="200"/>
+<br>
+<br>
+John C. Reilly, Sarah Silverman, Gal Gadot
+</p>
+    Monday - 12pm (12:00)
+    Tuesday - 12pm (12:00)
+    Wednesday - 6pm (18:00)
+    Thursday - 6pm (18:00)
+    Friday - 6pm (18:00)
+    Saturday - 12pm (12:00)
+    Sunday - 12pm (12:00)
+
+<details>
+<summary><strong>More details, show times and bookings</strong></summary>
+<p>
+    <div class="w3-panel w3-blue w3-card-4">
+      <strong>Ralph Breaks the Internet (2018)</strong>
+<br>
+    John C. Reilly, Sarah Silverman, Gal Gadot
+<br>
+<br>
+    Six years after the events of "Wreck-It Ralph," Ralph and Vanellope, now friends, discover a wi-fi router in their arcade, leading them into a new adventure.
+<br>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/T73h5bmD8Dc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<p>
+<strong>Book now:</strong>
+<br>
+ <input id="mbutton" type="button" value="Monday - 12pm (12:00)" />
+ <input id="mbutton" type="button" value="Tuesday - 12pm (12:00)" />
+ <input id="mbutton" type="button" value="Wednesday - 6pm (18:00)" />
+ <input id="mbutton" type="button" value="Thursday - 6pm (18:00)" />
+ <input id="mbutton" type="button" value="Friday - 6pm (18:00)" />
+ <input id="mbutton" type="button" value="Saturday - 12pm (12:00)" >
+ <input id="mbutton" type="button" value="Sunday - 12pm (12:00)" >
+    </details>
+</div>
+</p>
+<div class="w3-panel w3-blue w3-card-4">
+    <strong>Boy Erased</strong>
+<br>
+    <img src='../../media/be.png' height="320" width="200"/>
+<br>
+<br>
+Lucas Hedges, Nicole Kidman, Joel Edgerton
+</p>
+    Wednesday - 12pm (12:00)
+    Thursday - 12pm (12:00)
+    Friday - 12pm (12:00)
+    Saturday - 12pm (12:00)
+    Sunday - 9pm (21:00)
+<details>
+    <summary><strong>More details, show times and bookings</strong></summary>
+<br>
+    Lucas Hedges, Nicole Kidman, Joel Edgerton
+<br>
+    The son of a Baptist preacher is forced to participate in a church-supported gay conversion program after being forcibly outed to his parents.
+<br>
+<br>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/-B71eyB_Onw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<p/>
+<strong>Book now:</strong><a name="aboutus"></a>
+<br>
+ <input id="mbutton" type="button" value="Wednesday - 12pm (12:00)" />
+ <input id="mbutton" type="button" value="Thursday - 12pm (12:00)" />
+ <input id="mbutton" type="button" value="Friday - 12pm (12:00)" />
+ <input id="mbutton" type="button" value="Saturday - 9pm (21:00)" />
+ <input id="mbutton" type="button" value="Sunday - 9pm (21:00)" />
+</details>
+    </section>
+    </div>
+
+<div class="booking">
+    <section>
+        <a name="booking"></a>
+<br>
+<h1>Booking</h1>
+    <form action="https://titan.csit.rmit.edu.au/~e54061/wp/lunardo-formtest.php" method="post">
+        <div id="orderform">
+             <input id="mbutton" name="movie[id]" type="hidden" value="xm234jq">
+             <input id="mbutton" name="movie[day]" type="hidden" value="xm234jq">
+             <input id="mbutton" name="movie[hour]" type="hidden" value="xm234jq">
+             <label for="custname">Enter your name: </label> <input id="cust[name]" name="cust[name]" required="" type="text" />
+		<br>
+		<br>
+             <label for="cust[email]">	Enter your email: </label> <input id="cust[email]" name="cust[email]" required="" type="email" />
+		<br>
+		<br>
+   	    Mobile number: <input maxlength="10" name="cust[mobile]" required="" type="tel" />
+		<br>
+		<br>
+            <p><label for="Standard Adult">Standard Adult:</label> <input id="seats[STA]" list="seats[STA]" name="seats[STA]" type="number" />
+		<br>
+            <p><label for="Standard Concessiont">Standard Concession:</label> <input id="seats[STP]" list="seats[STP]" name="seats[STP]" type="number" /
+		<br>
+            <p><label for="Standard Child">Standard Child:</label> <input id="seats[STC]" list="seats[STC]" name="seats[STC]" type="number" />
+		<br>
+            <p><label for="First Class Adult">First Class Adult:</label> <input id="seats[FCA]" list="seats[FCA]" name="seats[FCA]" type="number" />
+		<br>
+            <p><label for="First Class Concessiont">First Class Concession:</label> <input id="seats[FCP]" list="seats[FCP]" name="seats[FCP]" type="number" />
+		<br>
+            <p><label for="First Class Child">First Class Child:</label> <input id="seats[FCC]" list="seats[FCC]" name="seats[FCC]" type="number" />
+	    <p>
+            <label for="Credit Card">Credit card</label> <input id="cust[card]" maxlength="19" name="cust[card]" required="" type="text" />
+		<br>
+		<br>
+            <label for="Expiry">Expiry</label> <input id="cust[expiry]" min="01-2019" name="cust[expiry]" type="month" value="01-2019" />
+        </div>
+</p>
+<div class="order" id="ordersubmit"><input type="submit" value="Order"/></div>
+<br/>
+
+    </form>
 
     <main>
       <article id='Website Under Construction'>
@@ -31,10 +324,11 @@
     <footer>
       <div>&copy;<script>
         document.write(new Date().getFullYear());
-      </script> Put your name(s), student number(s) and group name here. Last modified <?= date ("Y F d  H:i", filemtime($_SERVER['SCRIPT_FILENAME'])); ?>.</div>
+      </script> Andrew Nielsen, S3770419. Gitbucket: https://github.com/s3770419/wp. Last modified <?= date ("Y F d  H:i", filemtime($_SERVER['SCRIPT_FILENAME'])); ?>.</div>
       <div>Disclaimer: This website is not a real website and is being developed as part of a School of Science Web Programming course at RMIT University in Melbourne, Australia.</div>
       <div><button id='toggleWireframeCSS' onclick='toggleWireframe()'>Toggle Wireframe CSS</button></div>
     </footer>
+    </div>
 
   </body>
 </html>
